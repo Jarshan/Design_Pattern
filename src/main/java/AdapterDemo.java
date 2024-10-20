08a293e64369db716953f4a66e3bac91ff1bb882
Jarshan <jarshanjauffar@gmail.com>
2024-10-20 12:29:50 +0530

    AudioPlayers implements MediaPlayers

--- src/main/java/AdapterDemo.java
+++ src/main/java/AdapterDemo.java
@@ -36,3 +36,20 @@ class MediaAdapters implements MediaPlayers {
         }
     }
 }
+
+
+// Concrete class implementing the target interface
+class AudioPlayers implements MediaPlayers {
+    MediaAdapters mediaAdapter;
+
+    public void play(String audioType, String fileName) {
+        if (audioType.equalsIgnoreCase("mp3")) {
+            System.out.println("Playing mp3 file. Name: " + fileName);
+        } else if (audioType.equalsIgnoreCase("vlc") || audioType.equalsIgnoreCase("mp4")) {
+            mediaAdapter = new MediaAdapters(audioType);
+            mediaAdapter.play(audioType, fileName);
+        } else {
+            System.out.println("Invalid media. " + audioType + " format not supported");
+        }
+    }
+}
