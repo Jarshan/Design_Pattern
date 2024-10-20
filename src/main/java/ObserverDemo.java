4c0ae30283223fdd5813f88278066c3de2b21a18
Jarshan <jarshanjauffar@gmail.com>
2024-10-20 12:35:56 +0530

    WeatherStation example

--- src/main/java/ObserverDemo.java
+++ src/main/java/ObserverDemo.java
@@ -1,2 +1,37 @@
 import java.util.ArrayList;
 import java.util.List;
+
+// Subject
+class WeatherStation {
+    private List<Observer> observers = new ArrayList<>();
+    private float temperature;
+    private float humidity;
+
+    public float getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(float temperature) {
+        this.temperature = temperature;
+        notifyAllObservers();
+    }
+
+    public float getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(float humidity) {
+        this.humidity = humidity;
+        notifyAllObservers();
+    }
+
+    public void attach(Observer observer) {
+        observers.add(observer);
+    }
+
+    public void notifyAllObservers() {
+        for (Observer observer : observers) {
+            observer.update();
+        }
+    }
+}
